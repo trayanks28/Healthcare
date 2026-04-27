8ff424ef95f2cc77cbc620e114a7304947fb1fce
trayanks28 <trayanksahu8827@gmail.com>
2026-04-27 13:40:53 +0530

    Initial commit with .gitignore

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = ''; // XAMPP default is empty
+$database = 'healthcare';
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
